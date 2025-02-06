13da1f8c54079535eb202faa1af9c6cbf1c6f3c5
Felipe <felipesoares0405@gmail.com>
2025-02-06 17:38:25 -0300

    FoodExplorer API Backend production version

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://sitefoodexplorer.netlify.app"],
   credentials: true
 }))
 
